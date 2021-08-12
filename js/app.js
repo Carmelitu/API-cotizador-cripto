05dbe84eda1e5019bedb2b28ab90ece8d4c5c8d5
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-12 10:33:01 -0300

    se devuelven datos al enviar consulta ok

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const criptoSelect = document.querySelector('#criptomonedas');
 const monedaSelect = document.querySelector('#moneda');
 const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
 
 const objBusqueda = {
     moneda: '',
@@ -58,6 +59,9 @@ function verificarFormulario(e){
         return;
     }
 
+    // Consultar API
+    consultarAPI();
+
 }
 
 function mostrarAlerta(mensaje){
@@ -76,3 +80,48 @@ function mostrarAlerta(mensaje){
     }
 }
     
+function consultarAPI(){
+    const {moneda, cripto} = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => {
+            mostrarCotizacionHTML(cotizacion.DISPLAY[cripto][moneda]);
+        })
+}
+
+function mostrarCotizacionHTML(cotizacion){
+    limpiarHTML();
+    
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `Precio más alto del día: <span>${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `Precio más bajo del día: <span>${LOWDAY}</span>`;
+    
+    const ultimas24hs = document.createElement('p');
+    ultimas24hs.innerHTML = `Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const ultimaActualizacion = document.createElement('p');
+    ultimaActualizacion.innerHTML = `Última actualización': <span>${LASTUPDATE}</span>`;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimas24hs);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
