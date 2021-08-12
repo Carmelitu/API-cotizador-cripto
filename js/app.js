bafee535bde608acca87f0651a78685bda12bed3
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-12 09:42:01 -0300

    validacion de formulario agregada

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
+// Variables
 const criptoSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    cripto: ''
+}
 
 // Crear Promise
 const obtenerCripto = criptomonedas => new Promise(resolve => {
     resolve(criptomonedas);
 });
 
-document.addEventListener('DOMContentLoaded', consultarCripto);
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCripto();
+
+    formulario.addEventListener('submit', verificarFormulario);
+    criptoSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
+
+});
 
 
 function consultarCripto(){
@@ -28,3 +43,36 @@ function selectCripto(criptomonedas){
     });
 }
 
+function leerValor(e){
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function verificarFormulario(e){
+    e.preventDefault();
+    console.log(objBusqueda);
+
+    const {moneda, cripto} = objBusqueda;
+
+    if (moneda === '' || cripto === ''){
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+}
+
+function mostrarAlerta(mensaje){
+
+    const existeError = document.querySelector('.error');
+    if(!existeError){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('error');
+        divMensaje.textContent = mensaje;
+
+        formulario.appendChild(divMensaje);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+    
